3759d4650824da32c5060e4fc244e33c75f73b3f
anish-2508 <anish.virkhare7@gmail.com>
2024-03-05 09:46:03 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:7500", {
+            const socket = io("https://mern-chat-app-btmy.onrender.com", {
                 query : {
                     userId : authUser._id
                 }
