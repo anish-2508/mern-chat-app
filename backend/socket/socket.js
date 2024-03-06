afe2cbd29fc867e83b56b6cd2668fa42a3dcf3b8
anish-2508 <anish.virkhare7@gmail.com>
2024-03-06 08:44:37 +0530

    socket url updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app= express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors : {
-        origin : ["http://localhost:4000"],
+        origin : ["https://mern-chat-app-btmy.onrender.com/"],
         methods : ["GET", "POST"]
     },
 })
